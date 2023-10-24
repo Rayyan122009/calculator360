ff91e1513df8b1cef6d9dfe4c04831adc07da55a
Rayyan122009 <148577428+Rayyan122009@users.noreply.github.com>
2023-10-24 10:09:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node 
+"use strict";
+// import inquirer from "inquirer"
+Object.defineProperty(exports, "__esModule", { value: true });
+// import { sum } from "./operations/add.js";
+// import { sub } from "./operations/sub.js";
+// import { div } from "./operations/divide.js";
+// import { mul } from "./operations/multply.js";
+// let answer = await inquirer.prompt([
+//     {
+//     message: "Enter your first the number",
+//     type: "number",
+//     name: "num1"
+//     },
+//     {
+//         message: "Enter your second the number",
+//         type: "number",
+//         name: "num2"
+//         },
+//      {
+//         message: 'Select operator',
+//         type: "list",
+//         choices: ["+","-","*","/"],
+//         name: "operator"
+//      }
+// ])
+// answer.num1
+// if (answer.operator === "+"){
+//     let result = sum (answer.num1, answer.num2)
+//     console.log (result)
+// }
+// else if (answer.operator === "-"){
+//     let result = sub (answer.num1,answer.num2)
+// console.log(result)}
+// else if (answer.operator === "/"){
+//     let result = div (answer.num1,answer.num2)
+// console.log(result)}
+// else if (answer.operator === "*"){
+//     let result = mul (answer.num1,answer.num2)
+// console.log(result)}
+var a = 5;
+a++;
+console.log(a);
---
+++ operations/divide.ts
@@ -0,0 +1,6 @@
+export function div(num1:number, num2:number){
+    return num1 / num2
+    }
+    
+    
+    
